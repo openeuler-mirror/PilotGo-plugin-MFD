9bf0750aeaa0274412a0ed65da3ed9048b86d735
gyxforeveryoung <gyxforeveryoung@163.com>
2024-07-10 09:45:32 +0800

    获取node下所有zone的信息
    Signed-off-by: gyxforeveryoung <gyxforeveryoung@163.com>

--- src/fraginfo.py
+++ src/fraginfo.py
@@ -20,34 +20,60 @@ bpf_text = """
 #include <linux/mm.h>
 #include <linux/gfp.h>
 
-
-
-// 事件采集的数据
-struct data_t {
+// 区域信息结构
+struct zone_info {
+    u64 pgdat_ptr;
     u64 zone_ptr;
+    u64 zone_start_pfn;
+    u64 spanned_pages;
+    u64 present_pages;
+    char name[32];
+};
+
+// 内存节点信息结构
+struct pgdat_info {
     u64 pgdat_ptr;
     int nr_zones;
     int node_id;
 };
 
 // 定义 event
-BPF_PERF_OUTPUT(events);
-
-
+BPF_PERF_OUTPUT(pgdat_events);
+BPF_PERF_OUTPUT(zone_events);
 
 int kprobe__rmqueue_bulk(struct pt_regs *ctx, struct zone *zone, unsigned int order, unsigned long count, struct list_head *list, int migratetype, unsigned int alloc_flags) {
-    struct data_t data = {};
+    struct pgdat_info pgdat_data = {};
+    struct zone_info zone_data = {};
     struct pglist_data *pgdat;
+    struct zone *z;
+    int i;
 
-    // 获取 pgdat 结构和信息
-    data.zone_ptr = (u64)zone;
+    // 获取 pgdat 结构及信息
     pgdat = zone->zone_pgdat;
-    data.pgdat_ptr = (u64)pgdat;
-    data.nr_zones = pgdat->nr_zones;
-    data.node_id = pgdat->node_id;
+    pgdat_data.pgdat_ptr = (u64)pgdat;
+    pgdat_data.nr_zones = pgdat->nr_zones;
+    pgdat_data.node_id = pgdat->node_id;
 
-    // 提交数据到用户空间
-    events.perf_submit(ctx, &data, sizeof(data));
+    // 提交 pgdat 数据到用户空间
+    pgdat_events.perf_submit(ctx, &pgdat_data, sizeof(pgdat_data));
+
+    // 采集并提交每个 zone 的信息
+    #pragma unroll
+    for (i = 0; i < MAX_NR_ZONES; i++) {  // 假设最多有4个zone
+        if (i >= pgdat_data.nr_zones) {
+            break;
+        }
+        z = &pgdat->node_zones[i];
+        zone_data.pgdat_ptr = (u64)pgdat;
+        zone_data.zone_ptr = (u64)z;
+        zone_data.zone_start_pfn = z->zone_start_pfn;
+        zone_data.spanned_pages = z->spanned_pages;
+        zone_data.present_pages = z->present_pages;
+        bpf_probe_read_kernel_str(&zone_data.name, sizeof(zone_data.name), z->name);
+
+        // 提交 zone 数据到用户空间
+        zone_events.perf_submit(ctx, &zone_data, sizeof(zone_data));
+    }
 
     return 0;
 }
@@ -56,19 +82,28 @@ int kprobe__rmqueue_bulk(struct pt_regs *ctx, struct zone *zone, unsigned int or
 # 加载 BPF 程序
 b = BPF(text=bpf_text)
 
-# 输出函数
-def print_event(cpu, data, size):
-    event = b["events"].event(data)
+# 定义输出回调函数
+def print_pgdat_event(cpu, data, size):
+    event = b["pgdat_events"].event(data)
     if isUMA:
         print("Your system is UMA!\n")
     else:
         print("Your system is NUMA\n")
 
     print("内存节点的信息：")
-    print("pgdat_ptr: 0x{:x}  nr_zones: {}  node_id: {}".format(event.pgdat_ptr, event.nr_zones, event.node_id))
+    print("pgdat_ptr: 0x{:x}  nr_zones: {}  node_id: {}  \n".format(event.pgdat_ptr, event.nr_zones, event.node_id))
+
+def print_zone_event(cpu, data, size):
+    event = b["zone_events"].event(data)
+    zone_name = event.name.decode('utf-8', 'replace').rstrip('\x00')
+    print(f"ZONE {zone_name}：")
+    print(" zone_start_pfn  包含的页 管理的页")
+    print(f"     {event.zone_start_pfn}          {event.spanned_pages}     {event.present_pages}")
 
 
-b["events"].open_perf_buffer(print_event)
+# 监听事件并输出结果
+b["pgdat_events"].open_perf_buffer(print_pgdat_event)
+b["zone_events"].open_perf_buffer(print_zone_event)
 
 print("Tracing... Press Ctrl-C to end.")
 

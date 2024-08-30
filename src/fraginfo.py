a954a5f1612c9933941dc02accfb0022351c657e
gyxforeveryoung <gyxforeveryoung@163.com>
2024-08-30 13:16:37 +0800

    fix click

--- src/fraginfo.py
+++ src/fraginfo.py
@@ -3,47 +3,43 @@ from bcc import BPF
 import os
 import time
 
-
 class ExtFrag:
-    def __init__(self, interval=5, output_score_a=False, output_score_b=False, output_count=False,isNUMA=False):
-        self.isNUMA =  self.is_numa()
+    def __init__(self, interval=5, output_score_a=False, output_score_b=False, output_count=False):
+        self.isNUMA = self.is_numa()
         self.interval = interval
         self.output_score_a = output_score_a
         self.output_score_b = output_score_b
         self.output_count = output_count
+
         if self.output_count:
             self.b = BPF(src_file="./bpf/extfraginfo.c")
         elif not self.isNUMA:
-            self.b=BPF(src_file="./bpf/fraginfo.c")
+            self.b = BPF(src_file="./bpf/fraginfo.c")
         else:
-            self.b=BPF(src_file="./bpf/numafraginfo.c")
+            self.b = BPF(src_file="./bpf/numafraginfo.c")
 
     def is_numa(self):
         nodes_path = "/sys/devices/system/node/"
         try:
             nodes = [node for node in os.listdir(nodes_path) if node.startswith("node")]
-            if len(nodes) > 1:
-                return True
+            return len(nodes) > 1
         except FileNotFoundError:
-            pass
-        return False
+            return False
+
     def calculate_scoreA(self, score_a):
         score_a_int_part = int(score_a) // 1000
         score_a_dec_part = int(score_a) % 1000
-        formatted_score_a = f"{score_a_int_part:2d}.{score_a_dec_part:03d}"
-        return formatted_score_a
+        return f"{score_a_int_part:2d}.{score_a_dec_part:03d}"
 
     def calculate_scoreB(self, score_b):
         score_b_int_part = int(score_b) // 1000
         score_b_dec_part = int(score_b) % 1000
-        formatted_score_b = f"{score_b_int_part:2d}.{score_b_dec_part:03d}"
-        return formatted_score_b
+        return f"{score_b_int_part:2d}.{score_b_dec_part:03d}"
 
     def get_zone_data(self):
-        zone_data_dict = {} 
-        zone_map = self.b["zone_map"]  
+        zone_data_dict = {}
+        zone_map = self.b["zone_map"]
 
-        # 遍历 BPF map 来收集数据
         for key, value in zone_map.items():
             comm = value.name.decode('utf-8', 'replace').rstrip('\x00')
             data = {
@@ -55,38 +51,36 @@ class ExtFrag:
                 'free_blocks_total': value.free_blocks_total,
                 'free_blocks_suitable': value.free_blocks_suitable,
                 'free_pages': value.free_pages,
-                'scoreA': self.calculate_scoreA(value.score_a) ,
-                'scoreB': self.calculate_scoreB(value.score_b)
+                'scoreA': self.calculate_scoreA(value.score_a),
+                'scoreB': self.calculate_scoreB(value.score_b),
+                'node_id': value.node_id  
             }
             if comm not in zone_data_dict:
                 zone_data_dict[comm] = []
             zone_data_dict[comm].append(data)
             for comm in zone_data_dict:
-                 zone_data_dict[comm].sort(key=lambda x: x['order'])
+                zone_data_dict[comm].sort(key=lambda x: x['order'])
 
         return zone_data_dict
+
     def get_node_data(self):
-        node_data_dict = {}  # 使用字典存储数据
+        node_data_dict = {}
         pgdat_map = self.b["pgdat_map"]
-        # 遍历 BPF map 来收集数据
+
         for key, value in pgdat_map.items():
             data = {
                 'pgdat_ptr': value.pgdat_ptr,
                 'nr_zones': value.nr_zones,
-                'node_id':value.node_id
+                'node_id': value.node_id
             }
             node_id = value.node_id
-            node_data_dict[node_id] = data  # 将数据存储到字典中，以 node_id 作为键
+            node_data_dict[node_id] = data
 
         return node_data_dict
-       
- # 定义输出回调函数
-    def print_event11(self,cpu, data, size):
+
+    def print_event(self, cpu, data, size):
         event = self.b["events"].event(data)
-
-        
         print(" %4d %10lu %12d %15d %18d %18d %18d" % (
-
             event.index,
             event.pfn,
             event.alloc_order,
@@ -95,6 +89,7 @@ class ExtFrag:
             event.fallback_migratetype,
             event.change_ownership,
         ))
+
     def get_event_data(self):
         events_data = []
 
@@ -120,14 +115,14 @@ class ExtFrag:
             except Exception as e:
                 print(f"An error occurred while collecting events: {e}")
 
-        return events_data  # 确保函数始终返回 events_data 列表
+        return events_data
 
     def run(self):
         if self.output_count:
             print("%-7s %-10s %-12s %-15s %-18s %-18s %-18s" % (
-         "CALLS", "PFN", "ALLOC_ORDER", "FALLBACK_ORDER", "ALLOC_MIGRATETYPE", "FALLBACK_MIGRATETYPE", "CHANGE_OWNERSHIP"
-        ))
-            self.b["events"].open_perf_buffer(self.print_event)  
+                "CALLS", "PFN", "ALLOC_ORDER", "FALLBACK_ORDER", "ALLOC_MIGRATETYPE", "FALLBACK_MIGRATYPE", "CHANGE_OWNERSHIP"
+            ))
+            self.b["events"].open_perf_buffer(self.print_event)
         while True:
             try:
                 self.b.perf_buffer_poll(timeout=100)
--- src/fraginfo_user.py
+++ src/fraginfo_user.py
@@ -15,7 +15,20 @@ palette = [
 column_explanations = {
     "Node ID": "The unique identifier of the memory node.",
     "Number of Zones": "The number of memory zones associated with this node.",
-    "PGDAT Pointer": "The memory address of the pgdat structure for this node."
+    "PGDAT Pointer": "The memory address of the pgdat structure for this node.",
+    "COMM": "The name of the memory zone, e.g., DMA, Normal.",
+    "ZONE_PFN": "Starting page frame number of the memory zone.",
+    "SUM_PAGES": "Total number of pages in the zone.",
+    "FACT_PAGES": "Number of pages in the zone currently in use.",
+    "ORDER": "The order of the allocation request.",
+    "TOTAL": "Total number of free blocks in this zone.",
+    "SUITABLE": "Number of blocks suitable for the allocation.",
+    "FREE": "Total number of free pages in the zone.",
+    "SCORE": "Score representing fragmentation (higher is worse).",
+    "SCORE1": "Score A: Score representing fragmentation based on metric A.",
+    "SCORE2": "Score B: Score representing fragmentation based on metric B.",
+    'NODE_ID':"zone is in this node",
+    "FRAG_BAR": "A visual representation of the fragmentation score."
 }
 
 # 定义不同数据值的解释
@@ -24,6 +37,8 @@ value_explanations = {
     "Number of Zones": lambda value: f"Number of memory zones: {value}",
     "PGDAT Pointer": lambda value: f"Memory address of pgdat structure: {value}",
 }
+
+all_node_data = []
 
 def show_tooltip(text):
     """ 显示提示信息 """
@@ -35,6 +50,18 @@ def show_tooltip(text):
     
     loop.widget = overlay
     loop.draw_screen()  # 确保立即重绘屏幕以显示提示框
+
+def add_or_update_node_data(node_data):
+    """通过node_id来确保节点信息的唯一性并更新全局数组"""
+    global all_node_data
+    for new_node in node_data:
+        existing_node = next((node for node in all_node_data if node['node_id'] == new_node['node_id']), None)
+        if existing_node:
+            # 如果已经存在该node_id的信息，更新数据
+            existing_node.update(new_node)
+        else:
+            # 如果是新node_id，添加到全局数组
+            all_node_data.append(new_node)
 
 def item_clicked(column, value):
     """ 处理点击事件 """
@@ -50,88 +77,91 @@ def handle_tooltip_input(key):
     """处理提示框输入，按任意键返回主界面"""
     loop.widget = main_widget  # 返回主界面
 
-def create_table(extfrag, args):
+def create_node_table(extfrag, args):
+    """构建并显示所有 Node 信息"""
     rows = []
     
-    if args.node_info:
-        # 输出 Node 信息
-        header = ["Node ID", "Number of Zones", "PGDAT Pointer"]
-        header_widgets = [urwid.AttrMap(ClickableText(urwid.Text(col, align='center'), col, col), 'header') for col in header]
-        header_row = urwid.Columns(header_widgets)
-        rows.append(header_row)
+    # 定义表头
+    header = ["Node ID", "Number of Zones", "PGDAT Pointer"]
+    header_widgets = [urwid.AttrMap(urwid.Text(col, align='center'), 'header') for col in header]
+    header_row = urwid.Columns(header_widgets)
+    rows.append(header_row)  # 添加表头行
+    
+    # 获取节点数据并更新全局数组
+    node_data = extfrag.get_node_data()
+    add_or_update_node_data(node_data.values())
 
-        node_data = extfrag.get_node_data()
-        sorted_node_ids = sorted(node_data.keys())
-        
-        for node_id in sorted_node_ids:
-            node = node_data[node_id]
-            columns = ["node_id", "nr_zones", "pgdat_ptr"]
+    # 遍历全局数组中的所有节点并将其数据添加到表格行中
+    for node in all_node_data:
+        cell_widgets = [
+            urwid.Text(str(node['node_id']), align='center'),
+            urwid.Text(str(node['nr_zones']), align='center'),
+            urwid.Text(str(node['pgdat_ptr']), align='center')
+        ]
+        row = urwid.Columns(cell_widgets)
+        rows.append(row)  # 将每个节点的信息添加为一行
+
+    # 将所有行组合成一个表格
+    table = urwid.Pile(rows)
+    table_with_scroll = urwid.Filler(table, valign='top')
+    return table_with_scroll
+
+def create_zone_table(extfrag, args):
+    """构建并显示Zone信息"""
+    rows = []
+    if args.score_a:
+        header = ["COMM", "ZONE_PFN", "SUM_PAGES", "FACT_PAGES", 
+                  "ORDER", "TOTAL", "SUITABLE", "FREE", "SCORE1","NODE_ID"]
+        if args.bar:
+            header.append("FRAG_BAR")
+    elif args.score_b:
+        header = ["COMM", "ZONE_PFN", "SUM_PAGES", "FACT_PAGES", 
+                  "ORDER", "TOTAL", "SUITABLE", "FREE", "SCORE2","NODE_ID"]
+        if args.bar:
+            header.append("FRAG_BAR")
+    else:
+        header = ["COMM", "ZONE_PFN", "SUM_PAGES", "FACT_PAGES", 
+                  "ORDER", "TOTAL", "SUITABLE", "FREE", "SCORE1", "SCORE2","NODE_ID"]
+        if args.bar:
+            header.append("FRAG_BAR")
+
+    header_widgets = [urwid.AttrMap(ClickableText(urwid.Text(col, align='center'), col, col), 'header') for col in header]
+    header_row = urwid.Columns(header_widgets)
+    rows.append(header_row)
+
+    zone_data = extfrag.get_zone_data()
+
+    for comm, zones in zone_data.items():
+        for zone in zones:
+            if args.node_id is not None and zone.get('node_id') != args.node_id:
+                continue
+            if args.comm is not None and zone.get('comm') != args.comm:
+                continue
+            if args.score_a:
+                columns = ["comm", "zone_pfn", "spanned_pages", "present_pages", 
+                           "order", "free_blocks_total", "free_blocks_suitable", "free_pages", "scoreA","node_id"]
+            elif args.score_b:
+                columns = ["comm", "zone_pfn", "spanned_pages", "present_pages", 
+                           "order", "free_blocks_total", "free_blocks_suitable", "free_pages", "scoreB","node_id"]
+            else:
+                columns = ["comm", "zone_pfn", "spanned_pages", "present_pages", 
+                           "order", "free_blocks_total", "free_blocks_suitable", "free_pages", "scoreA", "scoreB","node_id"]
+
             cell_widgets = []
             for col in columns:
-                value = str(node.get(col, ""))
+                value = str(zone.get(col, ""))
                 text = urwid.Text(value, align='center')
                 clickable_text = urwid.AttrMap(ClickableText(text, col, value), 'body', focus_map='reversed')
                 cell_widgets.append(clickable_text)
+            if args.bar:
+                score = float(zone.get("scoreB" , 0))
+                frag_bar = generate_fragmentation_bar(score)
+                bar_widget = urwid.Text(frag_bar, align='center')
+                cell_widgets.append(bar_widget)
             row = urwid.Columns(cell_widgets)
             rows.append(row)
-    
-    else:
-        # 输出 Zone 信息
-        if args.score_a or args.score_b:
-            header = ["COMM", "ZONE_PFN", "SUM_PAGES", "FACT_PAGES", 
-                      "ORDER", "TOTAL", "SUITABLE", "FREE", "SCORE"]
-            if args.bar:
-                header.append("FRAG_BAR")
-        else:
-            header = ["COMM", "ZONE_PFN", "SUM_PAGES", "FACT_PAGES", 
-                      "ORDER", "TOTAL", "SUITABLE", "FREE", "SCORE1", "SCORE2"]
-            if args.bar:
-                header.append("FRAG_BAR")
 
-        header_widgets = [urwid.AttrMap(ClickableText(urwid.Text(col, align='center'), col, col), 'header') for col in header]
-        header_row = urwid.Columns(header_widgets)
-        rows.append(header_row)
-
-        zone_data = extfrag.get_zone_data()
-        
-        for comm, zones in zone_data.items():
-            if args.comm and comm != args.comm:
-                continue  
-            for zone in zones:
-                columns = ["comm", "zone_pfn", "spanned_pages", "present_pages", 
-                           "order", "free_blocks_total", "free_blocks_suitable", "free_pages"]
-                if args.score_a:
-                    columns.append("scoreA")
-                elif args.score_b:
-                    columns.append("scoreB")
-                else:
-                    columns.extend(["scoreA", "scoreB"])
-
-                # 根据条件设置颜色
-                color = 'body'
-                order = int(zone.get('order', 0))
-                scoreB = float(zone.get('scoreB', 0))
-                if order > 5 and scoreB > 0.5:
-                    color = 'highlight'
-
-                cell_widgets = []
-                for col in columns:
-                    value = str(zone.get(col, ""))
-                    text = urwid.Text(value, align='center')
-                    clickable_text = urwid.AttrMap(ClickableText(text, col, value), color, focus_map='reversed')
-                    cell_widgets.append(clickable_text)
-
-                if args.bar:
-                    score = float(zone.get("scoreA" if args.score_a else "scoreB", 0))
-                    frag_bar = generate_fragmentation_bar(score)
-                    bar_text = urwid.Text(frag_bar, align='center')
-                    cell_widgets.append(urwid.AttrMap(bar_text, color))
-                
-                row = urwid.Columns(cell_widgets)
-                rows.append(row)
-    
     table = urwid.Pile(rows)
-    # 添加外层滚动
     table_with_scroll = urwid.Filler(table, valign='top')
     return table_with_scroll
 
@@ -139,15 +169,18 @@ def generate_fragmentation_bar(score, max_length=20):
     """生成用于显示碎片化程度的条形图"""
     proportion = min(max(score, 0), 1)
     bar_length = int(proportion * max_length)
-    return '[' + '#' * bar_length + '-' * (max_length - bar_length) + ']'
+    return  '#' * bar_length + '-' * (max_length - bar_length) 
 
 def refresh_data(loop, user_data):
     """刷新数据并更新界面"""
     global main_widget
     extfrag, args = user_data
-    main_widget = create_table(extfrag, args)
+    if args.node_info:
+        main_widget = create_node_table(extfrag, args)
+    else:
+        main_widget = create_zone_table(extfrag, args)
     loop.widget = main_widget
-    loop.set_alarm_in(args.delay, refresh_data, user_data)  # 再次设置定时器
+    loop.set_alarm_in(args.delay, refresh_data, user_data) 
 
 def handle_input(key):
     """处理键盘输入以关闭提示框"""
@@ -158,7 +191,6 @@ def handle_input(key):
             raise urwid.ExitMainLoop()
 
 class ClickableText(urwid.WidgetWrap):
-    """自定义可点击文本Widget"""
     def __init__(self, text_widget, column, value):
         super().__init__(text_widget)
         self.column = column
@@ -179,28 +211,33 @@ class ClickableText(urwid.WidgetWrap):
 def main():
     try:
         parser = argparse.ArgumentParser(description='Watch memory fragmentation with real-time updates')
-        parser.add_argument('-d', '--delay', type=int,  help='Delay between updates in seconds')
+        parser.add_argument('-d', '--delay', type=int, help='Delay between updates in seconds', default=2)
         parser.add_argument('-n', '--node_info', action='store_true', help='Output node information')
+        parser.add_argument('-i', '--node_id', type=int, help='Specify Node ID to get zone information')
         parser.add_argument('-c', '--comm', type=str, help='Filter by comm name')
         parser.add_argument('-e', '--score_a', action='store_true', help='Only output score_a')
         parser.add_argument('-u', '--score_b', action='store_true', help='Only output score_b')
         parser.add_argument('-s', '--output_count', action='store_true', help='Output fragmentation count')
-        parser.add_argument('-b', '--bar', action='store_true', help='Display fragmentation bar')  
+        parser.add_argument('-b', '--bar', action='store_true', help='Display fragmentation bar')
         args = parser.parse_args()
 
         extfrag = ExtFrag(interval=args.delay if args.delay else 0, output_count=args.output_count,
                           output_score_a=args.score_a, output_score_b=args.score_b)
 
         global loop, main_widget
-        main_widget = create_table(extfrag, args)
+
+        if args.node_info:
+            main_widget = create_node_table(extfrag, args)
+        else:
+            main_widget = create_zone_table(extfrag, args)
+
         loop = urwid.MainLoop(main_widget, palette, unhandled_input=handle_input)
 
-        # 设置定时刷新
         loop.set_alarm_in(args.delay, refresh_data, (extfrag, args))
 
         loop.run()
     except KeyboardInterrupt:
-        pass  # 捕获Ctrl+C并安静地退出
+        pass  
 
 if __name__ == "__main__":
     main()
